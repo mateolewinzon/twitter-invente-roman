ad3b0edb97ce057d050c6ee52aff88b110c5d2b2
Mateo <mateolewinzon@gmail.com>
2021-02-23 12:54:07 -0300

    Duplicated tweets issue

--- main.py
+++ main.py
@@ -11,38 +11,29 @@ logger = logging.getLogger()
 frases_no = ['No.', 'No jugó Román.']
 frases_si = ['Sí.', 'Jugó Román.', '¡Invente Román invente!']
 
-def check_mentions(api, since_id, frase):
+def check_mentions(api, since_id):
   logger.info('Checking mentions')
   new_since_id = since_id
   for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
     new_since_id = max(tweet.id, new_since_id)
     logger.info(f"answerng to {tweet.user.name}")
-    api.update_status(status=frase, in_reply_to_status_id=tweet.id)
-
-
+    api.update_status(status=get_phrase(api), in_reply_to_status_id=tweet.id)
   return new_since_id
 
-def get_phrase(last):
+def get_phrase(api):
   if random.random() < 0.8:
     frase = random.choice(frases_si)
-    if frase == last:
-      return get_phrase(frase)
-    return frase
-  else:
-    frase = random.choice(frases_no)
-    if frase == last:
-      return get_phrase(last)
-    return frase
+  else: frase =  random.choice(frases_no)
+  if api.home_timeline(count=1)[0].text == frase:
+    return get_phrase(api)
+  else: return frase
 
 def main():
-  frase = ''
-
   api = create_api()
   since_id = 1
   while True:
-    frase = get_phrase(frase)
-    since_id = check_mentions(api, since_id, frase)
+    since_id = check_mentions(api, since_id)
     logger.info("Waiting...")
-    time.sleep(60*5)
+    time.sleep(60)
 
 main()
