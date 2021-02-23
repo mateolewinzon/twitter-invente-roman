c199461fdf1b312fa021c8048ab0c9f0ec4e8ebe
Mateo <mateolewinzon@gmail.com>
2021-02-23 11:57:34 -0300

    Added bot

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+import random
+import tweepy
+import os
+import logging
+import time
+from config import create_api
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+frases_no = ['No.', 'No jugó Román.']
+frases_si = ['Sí.', 'Jugó Román.', '¡Invente Román invente!']
+
+def check_mentions(api, since_id):
+  logger.info('Checking mentions')
+  new_since_id = since_id
+  for tweet in tweepy.cursor(api.mentions_timeline, since_id=since_id).items():
+    new_since_id = max(tweet.id, new_since_id)
+    logger.info(f"answerng to {tweet.user.name}")
+    api.update_status(status=get_phrase(), in_reply_to_status_id=tweet.id)
+
+
+  return new_since_id
+
+def get_phrase():
+  if random.random() < 0.8:
+    return random.choice(frases_si)
+  else: return random.choice(frases_no)
+
 def main():
-  print('App running successfully')
+  api = create_api()
+  since_id = 1
+  while True:
+    since_id = check_mentions(api, since_id)
+
 main()
